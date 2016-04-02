2a6206f6c70c8496d80e676a4e92b7c4a8efca99
Christopher Hall <christopher.hall.3609@gmail.com>
2016-04-03 08:26:01 +1000

    Created Screen class with render and clear methods Added fps/ups counter

--- src/rain/Game.java
+++ src/rain/Game.java
@@ -1,7 +1,6 @@
 package rain;
 
 import java.awt.Canvas;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
@@ -10,16 +9,21 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import rain.graphics.Screen;
+
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
+	public static String title = "Rain";
 
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
+	
+	private Screen screen;
 
 	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
@@ -28,6 +32,7 @@ public class Game extends Canvas implements Runnable {
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
 
+		screen = new Screen(width, height);
 		frame = new JFrame();
 	}
 
@@ -47,10 +52,31 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	public void run() {
+		long lastTime = System.nanoTime(), timer = System.currentTimeMillis();
+		final double NS = 1000000000.0 / 60.0;
+		double delta = 0;
+		int frames = 0, updates = 0;
 		while (running) {
-			update();
+			long now = System.nanoTime();
+			delta += (now - lastTime) / NS;
+			lastTime = now;
+			while (delta >= 1) {
+				update();
+				updates++;
+				delta--;
+			}
 			render();
+			frames++;
+			
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println(updates + " UPS " + frames + " FPS");
+				frame.setTitle(title + " | " + updates + " UPS " + frames + " FPS");
+				updates = 0;
+				frames = 0;
+			}
 		}
+		stop();
 	}
 
 	private void update() {
@@ -63,10 +89,16 @@ public class Game extends Canvas implements Runnable {
 			createBufferStrategy(3);
 			return;
 		}
+		
+		screen.clear();
+		screen.render();
+		
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
 
 		Graphics g = bs.getDrawGraphics();
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
 		bs.show();
 	}
@@ -74,7 +106,7 @@ public class Game extends Canvas implements Runnable {
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.frame.setResizable(false);
-		game.frame.setTitle("Rain");
+		game.frame.setTitle(title);
 		game.frame.add(game);
 		game.frame.pack();
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
---
+++ src/rain/graphics/Screen.java
@@ -0,0 +1,28 @@
+package rain.graphics;
+
+public class Screen {
+	private int width, height;
+	public int[] pixels;
+	
+	public Screen(int width, int height) {
+		this.width = width;
+		this.height = height;
+		pixels = new int[width * height]; //50400
+	}
+	
+	public void clear() {
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0;
+		}
+	}
+	
+	public void render() {
+		for (int y = 0; y < height; y++) {
+			if (y < 0 || y >= height) break;
+			for (int x = 0; x < width; x++) {
+				if (x < 0 || x >= width) break;
+				pixels[x + y * width] = 0xff00ff;
+			}
+		}
+	}
+}
